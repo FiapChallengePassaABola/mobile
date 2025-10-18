03c127474cdf3cc400bd258039a3ee25d2e3c081
Paulo Vieira <paulohenriqueavieira@gmail.com>
2025-10-18 13:43:22 -0300

    Update: cores do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primaria: '#704674',
+        secundaria: '#118C65',
+        terciaria: '#980453'
+      }
+    },
   },
   plugins: [],
 }
