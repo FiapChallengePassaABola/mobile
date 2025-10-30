da30a88bd12c0483e1a49169d8d39fe8415eb685
Paulo Vieira <paulohenriqueavieira@gmail.com>
2025-10-30 18:42:28 -0300

    Update: cor de fundo landing page

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,11 @@ import { Text, View, Image, TouchableOpacity } from "react-native";
 import BackgroundScreen from "./components/BackgroundScreen";
 import { navigate } from "expo-router/build/global-state/routing";
 import "./globals.css"
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
   return (
-    <BackgroundScreen>
+    <LinearGradient colors={['#6A1B6C','#350E36']} locations={[0.11, 0.92]} start={{x:0,y:0}} end={{x:1, y:1}} className="w-screen h-screen justify-center items-center">
         <View className="flex-1 justify-end items-center gap-4">
           <Image source={require('../assets/icons/logo.png')}/>
           <Text className="font-semibold text-4xl color-white">PASSA A BOLA EVO</Text>
@@ -18,6 +19,6 @@ export default function Index() {
             <Text className="text-center color-white">já tenho uma conta</Text>
           </TouchableOpacity>
         </View>
-    </BackgroundScreen>
+    </LinearGradient>
   );
 }
