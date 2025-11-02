0012999061c3b1d067ae1194a07630bf3f92e88e
Paulo Vieira <paulohenriqueavieira@gmail.com>
2025-11-02 13:40:00 -0300

    Update: nova exibição de usuário na aba profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Image, ImageSourcePropType, ScrollView, Text, View } from "react-native";
+import {
+  Image,
+  ImageSourcePropType,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ActionIcon from "../components/ActionIcon";
 import BackgroundScreen from "../components/BackgroundScreen";
@@ -8,41 +14,63 @@ import { icons } from "./_layout";
 type avatarProps = {
   avatar: ImageSourcePropType;
   username: string;
+  position: string[];
+  weight: number;
+  height: number;
 };
 
-const Avatar = ({ avatar, username }: avatarProps) => {
+const Avatar = ({
+  avatar,
+  username,
+  position,
+  weight,
+  height,
+}: avatarProps) => {
   return (
-    <View className="w-full h-44 flex justify-center items-center gap-2">
-      <Image source={avatar} className="size-32" />
-      <Text className="color-white text-4xl font-bold">{username}</Text>
-    </View>
-  );
-};
-
-type estatisticaProps = {
-  status: number;
-  label: string;
-};
-const Estatisticas = ({ status, label }: estatisticaProps) => {
-  return (
-    <View className="w-24 h-24 flex justify-center items-center bg-primaria rounded-3xl p-2">
-      <Text className="color-white text-4xl font-semibold">{status}</Text>
-      <Text className="color-white font-semibold text-xs">{label}</Text>
+    <View className="w-screen h-52 flex-row justify-center items-center px-4">
+      <Image source={avatar} className="size-[7rem]" />
+      <View className="flex flex-1 h-[70%] p-2 ml-4 justify-center">
+        <Text className="color-white text-4xl font-medium">{username}</Text>
+        <Text className="color-white/60 text-lg">
+          posição: {position.join("/")}
+        </Text>
+        <Text className="color-white/60 text-lg">peso: {weight} kg</Text>
+        <Text className="color-white/60 text-lg">altura: {height} cm</Text>
+      </View>
     </View>
   );
 };
 
 const profile = () => {
-  const funcao = () => {
-    return 1 + 1;
+  const user = {
+    name: "Rafaela Leão",
+    position: ["zag", "vol"],
+    weight: 45,
+    height: 175,
   };
-
   return (
     <BackgroundScreen>
       <SafeAreaView className="w-screen h-screen">
-        <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: 175 }}
-        className="w-screen"> 
-          <ActionIcon icon={icons.config} onPress={()=>{return 1+1}} style="w-full flex items-end justify-center p-4"/>
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1, paddingBottom: 175 }}
+          className="w-screen"
+        >
+          <ActionIcon
+            icon={icons.config}
+            onPress={() => {
+              return 1 + 1;
+            }}
+            style="w-full flex items-end justify-center p-4"
+          />
+          <View className="w-screen flex justify-center items-center">
+            <Avatar
+              avatar={icons.profile}
+              username={user.name}
+              position={user.position}
+              weight={user.weight}
+              height={user.height}
+            />
+          </View>
         </ScrollView>
       </SafeAreaView>
     </BackgroundScreen>
